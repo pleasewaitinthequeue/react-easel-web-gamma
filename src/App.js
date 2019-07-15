07404fa7dbcd472c280e285ec2c9a28744470efe
Lairson, John Matthew <jomalair@iu.edu>
2019-07-15 14:15:41 -0400

    added some boilerplate, and installed dependencies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>EASEL</h1>
+        <h2>Educational Application Supported Experiential Learning</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
